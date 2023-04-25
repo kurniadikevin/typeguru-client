4a041475766c6a85946a932f9b0fcf0e33ac9387
kurniadikevin <kurniadikevin@gmail.com>
2023-04-25 16:35:22 +0700

    add end point and developt timing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,50 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
-  const [point,setPoint]=useState(0)
 
-
-  // check if the type character is same as the sample text
- const typeCheck=()=>{
-  
- }
+  const [status,setStatus]= useState('true');
+  const [index,setIndex]= useState(0);
+  const [time,setTime]= useState(0)
 
   const sampleText: string='The quick brown fox jumps over the lazy dog';
-  
+
+  // check if the type character is same as the sample text
+ const typeCheck=(event: any)=>{
+  if(event.key === sampleText[index]){
+    setIndex(()=> index+ 1)
+    setStatus('true');
+  } else if( index === sampleText.length ){
+    alert('game stop');
+  } else{
+    setStatus('false');
+  }
+ }
+
+  const incrementTime=()=>{
+    setTime(()=> time + 1)
+    console.log('time' + time)
+  }
+
+  const startTimer=()=>{
+    setInterval(incrementTime,1000);
+  }
+ 
+
 
 
   return (
     <main>
       Home
+      <div>{time}</div>
       <div>{sampleText}</div>
-      <textarea></textarea>
+      <textarea onKeyDown={typeCheck} ></textarea>
+      <div>{status}</div>
+      <button onClick={startTimer}>Plus</button>
     </main>
   )
 }
