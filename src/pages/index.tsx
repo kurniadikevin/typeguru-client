9b7b493a83e2611d20fb3a5432f29ad9cb74605c
kurniadikevin <kurniadikevin@gmail.com>
2023-04-26 21:20:49 +0700

    make timer and game restart cycle manually

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,44 +7,86 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
 
 
-  const [status,setStatus]= useState('true');
+  const [status,setStatus]= useState('null');
   const [index,setIndex]= useState(0);
-  const [time,setTime]= useState(0)
+  const [error,setError]= useState<number>(0);
+  //const [timeCount,setTimeCount]= useState(false);
+  const [timeElapse,setTimeElapse]= useState<number>(0);
+  //const[ inter,setInter]= useState(true);
+  const [intervalId,setIntervalId]= useState<any>();
+  const [playOn,setPlayOn]= useState(true)
+  let count: number=0;
 
-  const sampleText: string='The quick brown fox jumps over the lazy dog';
 
-  // check if the type character is same as the sample text
+  const sampleText: string='hello world';
+
+// check if the type character is same as the sample text
  const typeCheck=(event: any)=>{
+  // typing true
   if(event.key === sampleText[index]){
     setIndex(()=> index+ 1)
     setStatus('true');
-  } else if( index === sampleText.length ){
-    alert('game stop');
-  } else{
+    // typing finish
+  } else if( index === sampleText.length && playOn === true){
+    const grossWpm= (sampleText.length / 5  )/ (timeElapse / 60); 
+    alert('game stop in'+ (timeElapse));
+    alert('grossWpm : '+ grossWpm);
+    alert( 'error :' + error);
+    setStatus('finish');
+    setPlayOn(false);
+    clearInterval(intervalId);// late finish game one keydown after finish
+  } 
+  // typing condition false
+  else{
     setStatus('false');
+    setError(()=> error + 1);
   }
  }
 
-  const incrementTime=()=>{
-    setTime(()=> time + 1)
-    console.log('time' + time)
-  }
+const increaseTimeElapse=()=>{
+  count++;
+  setTimeElapse(count);
+}
 
-  const startTimer=()=>{
-    setInterval(incrementTime,1000);
-  }
- 
+ const startGame=()=>{
+  const textInput: any= document.querySelector('#text-input');
+  textInput.value='';
+  textInput.focus()
+  setPlayOn(true);
+  setIndex(0);
+  setError(0);
+  clearInterval(intervalId)
+  const Interval=setInterval(increaseTimeElapse, 1000);
+  setIntervalId(Interval);
+  return (()=> clearInterval(Interval)) 
+}
+
+const stopGame=()=>{
+  console.log('stop')
+  clearInterval(intervalId)
+  setTimeElapse(0);
+}
+
+
+  useEffect(()=>{
+  
+  },[])
 
 
 
   return (
-    <main>
-      Home
-      <div>{time}</div>
-      <div>{sampleText}</div>
-      <textarea onKeyDown={typeCheck} ></textarea>
-      <div>{status}</div>
-      <button onClick={startTimer}>Plus</button>
-    </main>
+    <div className='h-full flex flex-col items-center justify-center border-2 gap-5'>
+      <div className='h-20'>
+        <h1>Typing test</h1>
+      </div>
+      <div>time elapse: {timeElapse}</div>
+      <div className='h-12'>{sampleText}</div>
+      <textarea onKeyDown={typeCheck} id='text-input'
+      className='text-black h-20 resize-none'></textarea>
+      <div > Status: {status}</div>
+      <button onClick={startGame}>start</button>
+      <button onClick={stopGame}> stop</button>
+    </div>
   )
 }
+
