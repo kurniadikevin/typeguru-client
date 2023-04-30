e861c5914fe8b23b00ea5c258274286017dd8f42
kurniadikevin <kurniadikevin@gmail.com>
2023-04-30 20:44:11 +0700

    edit color result css

--- src/components/results.tsx
+++ src/components/results.tsx
@@ -6,30 +6,30 @@ export const ResultComponent=(props:any)=>{
         <div >
         {props.wpm >0  ? 
         <div className='flex flex-col text-2xl w-100 gap-2 font-bold
-         bg-neutral-800 rounded-xl p-12 px-14 mb-5 text-[color:var(--highlightColor)]'>
+         bg-neutral-800 rounded-xl p-12 px-14 mb-5 '>
             <div className='flex gap-10 justify-between'>
                 <div>WPM</div>
-                <div>{props.wpm}</div>
+                <div  className='text-[color:var(--accent)]'>{props.wpm}</div>
             </div>
             <div className='flex gap-10 justify-between'>
                 <div>Gross Wpm</div>
-                <div>{props.grossWpm}</div>
+                <div className='text-[color:var(--accent)]'>{props.grossWpm}</div>
             </div>
             <div className='flex gap-10 justify-between'>
                 <div>Accuracy</div>
-                <div>{(props.accuracy).toFixed(1)}%</div>
+                <div className='text-[color:var(--accent)]'>{(props.accuracy).toFixed(1)}%</div>
             </div>
             <div className='flex gap-10 justify-between'>
                 <div>Error</div>
-                <div>{props.error}</div>
+                <div className='text-[color:var(--accent)]'>{props.error}</div>
             </div>
             <div className='flex gap-10 justify-between'>
                 <div>Time</div>
-                <div>{displayCorrectTime(props.time)}</div>
+                <div className='text-[color:var(--accent)]'>{displayCorrectTime(props.time)}</div>
             </div> 
             <div className='flex gap-10 justify-between'>
                 <div>Word Count</div>
-                <div>{props.wordCount}</div>
+                <div  className='text-[color:var(--accent)]'>{props.wordCount}</div>
             </div> 
         </div>
         : ''}
